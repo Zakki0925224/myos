12fb5f3b9028ea010ada963401c0f2146997a187
Zakki <zakki0925224@gmail.com>
2022-06-15 20:57:12 +0900

    [Add]: Added test files for sata hdd test

--- src/device/storage/ahci.rs
+++ src/device/storage/ahci.rs
@@ -1,13 +1,18 @@
+use core::ptr::read_volatile;
+
 use crate::{util::logger::{log_warn, log_debug}, device::{pci::{PciDevice, BaseAddressRegister}, PCI}, println};
 
 const PCI_AHCI_BASE_CLASS_CODE: u8 = 0x01;
 const PCI_AHCI_SUB_CLASS_CODE: u8 = 0x06;
+const PCI_AHCI_BASE_ADDR_INDEX: usize = 5;
+const MAX_PORT_COUNT: usize = 32;
 
 #[derive(PartialEq)]
 pub struct Ahci
 {
     is_init: bool,
-    pci_ahci_device: PciDevice
+    pci_ahci_device: PciDevice,
+    hba_mem_regs: HostBusAdapterMemoryRegisters
 }
 
 impl Ahci
@@ -16,7 +21,7 @@ impl Ahci
     {
         let pci_ahci_device = PciDevice::new();
 
-        return Ahci { is_init: false, pci_ahci_device };
+        return Ahci { is_init: false, pci_ahci_device, hba_mem_regs: HostBusAdapterMemoryRegisters::new() };
     }
 
     pub fn init(&mut self)
@@ -37,11 +42,56 @@ impl Ahci
             return;
         }
 
+        let base_addr = self.pci_ahci_device.get_base_addr(PCI_AHCI_BASE_ADDR_INDEX);
+
+        match base_addr
+        {
+            Some(BaseAddressRegister::MemoryAddress32Bit(addr)) => self.hba_mem_regs.init(addr),
+            _ =>
+            {
+                log_warn("AHCI BAR#5 not found");
+                return;
+            }
+        }
+
         self.is_init = true;
     }
 
     pub fn is_init(&self) -> bool
     {
         return self.is_init;
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+pub struct HostBusAdapterMemoryRegisters
+{
+    generic_host_ctrl_reg: u32,
+    vendor_specific_regs: u32,
+    control_regs: [u32; MAX_PORT_COUNT]
+}
+
+impl HostBusAdapterMemoryRegisters
+{
+    pub fn new() -> HostBusAdapterMemoryRegisters
+    {
+        return HostBusAdapterMemoryRegisters { generic_host_ctrl_reg: 0, vendor_specific_regs: 0, control_regs: [0; MAX_PORT_COUNT] };
+    }
+
+    pub fn init(&mut self, base_addr: u32)
+    {
+        for i in 0..50
+        {
+            println!("0x{:x}", self.read_mem_reg(base_addr, i));
+        }
+    }
+
+    fn read_mem_reg(&self, base_addr: u32, index: usize) -> u32
+    {
+        unsafe
+        {
+            let buffer = base_addr as *const u32;
+            return read_volatile(buffer.offset(index as isize * 32));
+        }
     }
 }
